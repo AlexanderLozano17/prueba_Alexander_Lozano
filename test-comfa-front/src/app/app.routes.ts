321b4d13436f39222b5a6da02a502ae3378f406b
Alexander Lozano <alexanderlozano950@gmail.com>
2025-07-03 18:36:24 -0500

    conf .gitignore

--- test-comfa-front/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {
-        path: 'personajes',
-        loadChildren: () => import('./feature/personaje/personaje.routes').then(m => m.PERSONAJE_ROUTES)
-    }
-];
